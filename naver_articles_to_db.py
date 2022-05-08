e047a8e46493cabb602f80db0421c322b21cd59f
jjinyeok <fun2314@naver.com>
2022-05-09 02:04:04 +0900

    DELETE 기능 추가

--- naver_articles_to_db.py
+++ naver_articles_to_db.py
@@ -1,5 +1,6 @@
 
 from db_conn import db
+from delete import delete
 
 import time
 start = time.time()
@@ -14,6 +15,9 @@ db = db
 
 # cursor 생성
 cursor = db.cursor()
+
+# INSERT 전에 DELETE
+delete(db)
 
 # article INSERT
 article_insert_sql = """
