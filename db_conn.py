4db96ea814f2de3a82bf7a47700624e3309212b3
jjinyeok <fun2314@naver.com>
2022-05-10 21:11:27 +0900

    Selenium 없이 크롤링

--- db_conn.py
+++ db_conn.py
@@ -1,10 +1,10 @@
 import pymysql
 
 # DB와 연결
-db = pymysql.connect(host='localhost',
-                        user='root',
+db = pymysql.connect(host='demo.csfp9hcywqnb.ap-northeast-2.rds.amazonaws.com',
+                        user='fun2314',
                         password='12345678',
-                        db='news_whale',
+                        db='demo',
                         charset='utf8',
                         port=3306
                     )
--- naver_articles.py
+++ naver_articles.py
@@ -1,30 +1,37 @@
 
 def extract_article_from_naver():
     
-    # 크롤링 외부 라이브러리
-    import naver_crawling
+    # # 크롤링 외부 라이브러리
+    # import naver_crawling
 
-    # 네이버 랭킹뉴스 - 언론사별 많이 본 뉴스
-    naver_ranking_news = 'https://news.naver.com/main/ranking/popularDay.naver'
+    # # 네이버 랭킹뉴스 - 언론사별 많이 본 뉴스
+    # naver_ranking_news = 'https://news.naver.com/main/ranking/popularDay.naver'
 
-    # chrome 드라이버 로드
-    driver = naver_crawling.set_chrome_driver()
+    # # chrome 드라이버 로드
+    # driver = naver_crawling.set_chrome_driver()
 
-    # 해당 URL을 브라우저(chrome)에서 띄움
-    driver.get(naver_ranking_news)
+    # # 해당 URL을 브라우저(chrome)에서 띄움
+    # driver.get(naver_ranking_news)
 
-    # 모든 랭킹기사 보기
-    naver_crawling.find_all_articles(driver)
+    # # 모든 랭킹기사 보기
+    # naver_crawling.find_all_articles(driver)
 
-    # 기사 리스트 초기화
-    # {기사제목, 기사URL}
-    article_list = naver_crawling.init_articles(driver)
+    # # 기사 리스트 초기화
+    # # {기사제목, 기사URL}
+    # article_list = naver_crawling.init_articles(driver)
 
-    # 브라우저 닫기
-    naver_crawling.close_browser(driver)
+    # # 브라우저 닫기
+    # naver_crawling.close_browser(driver)
 
-    # 기사 리스트 전체 가지고 옴
-    article_list = naver_crawling.complete_articles(article_list)
+    # # 기사 리스트 전체 가지고 옴
+    # article_list = naver_crawling.complete_articles(article_list)
+
+    # return article_list
+
+    import naver_crawling_no_selenium
+
+    article_list = naver_crawling_no_selenium.init_articles()
+
+    article_list = naver_crawling_no_selenium.complete_articles(article_list)
 
     return article_list
-
